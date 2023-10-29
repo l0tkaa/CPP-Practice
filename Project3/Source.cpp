442f49f6b570168c474fbf62083bb58d79c82e9a
l0tkaa <106283020+l0tkaa@users.noreply.github.com>
2023-10-29 16:22:44 -0500

    Added IO Project

---
+++ Project3/Source.cpp
@@ -0,0 +1,18 @@
+//Section 5
+//Basic I/O using cin and cout
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	cout << "Hello world!" << endl;
+
+// cout << "Hello";
+// cout << "World" << endl;
+// cout << "Hello world!" << endl;
+// 
+
+}
