62351d5c89e5fda1e581c0794ae8805493e3e07c
l0tkaa <106283020+l0tkaa@users.noreply.github.com>
2023-10-29 20:43:23 -0500

    .

---
+++ Global Variables/Source.cpp
@@ -0,0 +1,16 @@
+//Section 6
+//Global and local variables. 
+
+#include <iostream>
+
+using namespace std;
+int age{ 18 }; //Global Variable 
+
+
+int main() {  //Local is INSIDE of main(){
+
+	int age{16};
+	
+	cout << age << endl;
+
+}
--- Variables/Source.cpp
+++ Variables/Source.cpp
@@ -5,6 +5,19 @@ using namespace std;
 // This program will calculate the area of a room in square feet
 
 int main() {
+
+	int room_width (0);
+	int room_length(0);
+
+	cout << "Enter the width of the room: ";
+	cin >> room_width;
+
+	cout << "Enter the lenth of the room:";
+	cin >> room_length;
+
+	cout << "The area of the room is" << room_width * room_length << "square feet." << endl;
+
+
 	return 0;
 
 }
