16f9ad58c3906af24b785b7524814e3b4ca340fa
l0tkaa <106283020+l0tkaa@users.noreply.github.com>
2023-10-30 20:09:44 -0500

    Multidemensional Arrays

---
+++ Multidemensional Arrays/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int movie_rating[3][4];
+
+	//Element_Type array_name [dim1_size][dim2_size]
+
+	const int rows{ 3 }
+	const int cols{ 4 };
+	int movie_rating[rows][cols];
+
+	//movie - second index
+	//reviewer- first index 
+
+
+
+	return 0;
+
+}
