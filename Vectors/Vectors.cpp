01802a79e51705dbab69f73d39d713379d3d2a04
l0tkaa <106283020+l0tkaa@users.noreply.github.com>
2023-11-18 23:24:45 -0600

    declare vectors

--- Vectors/Vectors.cpp
+++ Vectors/Vectors.cpp
@@ -8,6 +8,19 @@ using namespace std;
 int main() {
 
 
+
+	vector<int> use_vector() {
+		std::vector<int> vec{ 10,20,30,40,50 };
+		vec.at(0) = 100;
+		vec.at(4) = 1000;
+		/*Declare a vector of integers named vec and initialize the vector to 10, 20, 30, 40, and 50
+
+		Modify the first element of the vector to be 100 and modify the last element of the vector to be 1000 .
+
+			The final vector should then be 100, 20, 30, 40, and 1000.
+			*/
+	}
+
 	vector <char> vowels; //empty
 	vector <char> vowels{ 5 }; // 5 initialized to zero
 	vector <char> vowels{ 'a', 'e', 'i', 'o', 'u' };
